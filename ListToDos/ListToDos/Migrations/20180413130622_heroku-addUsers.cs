b6a7dab728326f9cdda9e75e19e2cad883dda85d
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-13 15:07:19 +0200

    Migrate the database to heroku

---
+++ ListToDos/ListToDos/Migrations/20180413130622_heroku-addUsers.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace ListToDos.Migrations
+{
+    public partial class herokuaddUsers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Todos",
+                nullable: false,
+                oldClrType: typeof(long))
+                .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Id",
+                table: "Users",
+                nullable: false,
+                oldClrType: typeof(int))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Id",
+                table: "Todos",
+                nullable: false,
+                oldClrType: typeof(long))
+                .OldAnnotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn);
+        }
+    }
+}
