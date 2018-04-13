d6862eecb42d58929ed235874717570132808193
Pedro Gómez de la Garza <pedro_0797@hotmail.com>
2018-04-13 15:27:42 +0200

    Add user feature hard coded

--- ListToDos/ListToDos/Controllers/UserController.cs
+++ ListToDos/ListToDos/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ListToDos.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ListToDos.Controllers
@@ -10,11 +11,28 @@ namespace ListToDos.Controllers
     [Route("user")]
     public class UserController : Controller
     {
+        private TodoContext db;
+
+        public UserController(TodoContext db)
+        {
+            this.db = db;
+        }
+
         [Route("")]
         [Route("sign-in")]
         public IActionResult SignIn()
         {
             return View();
+        }
+
+        [Route("add-user")]
+        public void AddUser(string email, string password)
+        {
+            db.Users.Add(new User
+            {
+                Email = email,
+                Password = password
+            });
         }
     }
 }
